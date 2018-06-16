fb8deba54bf0a3519127954a87730535ab33222f
Driver <peplinskikrzysztof@gmail.com>
2018-06-16 18:47:35 +0200

    connection failed

--- src/main/java/pl/peplinski/zadanie/app/DAO/daoPerson/AdderPerson.java
+++ src/main/java/pl/peplinski/zadanie/app/DAO/daoPerson/AdderPerson.java
@@ -7,8 +7,8 @@ public class AdderPerson {
     public static void main(String[] args) {
         Person person = new Person();
 
-        person.setName("Krzychu");
-        person.setSurname("Pelinski");
+        person.setName("Kubus");
+        person.setSurname("Puchatek");
 
         PersonDAO.addPerson(person);
     }
